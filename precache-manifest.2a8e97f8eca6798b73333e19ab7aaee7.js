39f55965cf454a90199f0f3a87e59d6329509b46
Jason Bruno <jcwebhole@yahoo.com>
2019-08-02 23:17:02 +0800

    built

---
+++ precache-manifest.2a8e97f8eca6798b73333e19ab7aaee7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a82d80bcf6ccaf8d339c3a148374315b",
+    "url": "/react001/index.html"
+  },
+  {
+    "revision": "6e98bcab18b7cb0d2d4e",
+    "url": "/react001/static/css/main.50357c0b.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/react001/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "6e98bcab18b7cb0d2d4e",
+    "url": "/react001/static/js/main.b1443096.chunk.js"
+  },
+  {
+    "revision": "a07107c1ec84c5797203",
+    "url": "/react001/static/js/runtime~main.bebb4b66.js"
+  }
+]);
